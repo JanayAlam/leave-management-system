8adc9d88d4cfa45ee23d5b85c386a48855ee51b3
JanayAlam <dev.janay@outlook.com>
2023-07-23 09:19:19 +0600

    feat: Designed the signin and signup page

--- signin.php
+++ signin.php
@@ -17,7 +17,11 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>LeaveBoard - Sign in</title>
-        <link rel="stylesheet" href="./css/auth/signin.css" />
+        <style>
+            .form-label {
+                margin-bottom: 0rem !important;
+            }
+        </style>
     </head>
     <body>
         <!-- Header -->
@@ -27,32 +31,96 @@
 
         <!-- Content -->
         <div class="container">
-            <div class="row">
-                <form method="POST">
-                    <div class="mb-3">
-                        <label for="email" class="form-label"
-                            >Email address</label
+            <div class="card card-body mt-5">
+                <div class="row">
+                    <div class="col-md-6 col-sm-12">
+                        <div
+                            class="d-flex justify-content-center align-items-center"
                         >
-                        <input
-                            type="email"
-                            name="email"
-                            class="form-control"
-                            id="email"
-                        />
+                            <img
+                                src="./img/signin-side-img.jpg"
+                                alt="Login page side photo"
+                                class="w-100"
+                            />
+                        </div>
                     </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label"
-                            >Password</label
-                        >
-                        <input
-                            type="password"
-                            name="password"
-                            class="form-control"
-                            id="password"
-                        />
+                    <div class="col-md-6 col-sm-12">
+                        <div class="d-flex flex-column">
+                            <div class="align-items-center text-center">
+                                <img
+                                    src="./img/logo.png"
+                                    alt="Logo"
+                                    width="120px"
+                                    class="mt-2 mb-5"
+                                />
+                            </div>
+                            <div>
+                                <h2
+                                    class="fw-bold mb-0"
+                                    style="font-size: 1.2rem"
+                                >
+                                    Online Leave Management System
+                                </h2>
+                                <p class="text-muted">Signin to your account</p>
+                            </div>
+                            <form method="POST">
+                                <div class="mb-2">
+                                    <label for="email" class="form-label"
+                                        >Email address</label
+                                    >
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        class="form-control"
+                                        id="email"
+                                        placeholder="i.e. example@domain.com"
+                                    />
+                                </div>
+                                <div class="mb-2">
+                                    <label for="password" class="form-label"
+                                        >Password</label
+                                    >
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        class="form-control"
+                                        id="password"
+                                        placeholder="Your secret password"
+                                    />
+                                </div>
+                                <div class="form-check mb-2">
+                                    <input
+                                        class="form-check-input"
+                                        value=""
+                                        type="checkbox"
+                                        id="remember-me"
+                                        name="remember-me"
+                                    />
+                                    <label
+                                        class="form-check-label"
+                                        for="remember-me"
+                                    >
+                                        Remember me
+                                    </label>
+                                </div>
+                                <div class="mb-2">
+                                    <p class="text-muted">
+                                        Don't have any account?
+                                        <a href="signup.php" class="text-dark"
+                                            >Create a new one!</a
+                                        >
+                                    </p>
+                                </div>
+                                <button
+                                    type="submit"
+                                    class="form-control btn btn-dark text-uppercase"
+                                >
+                                    Sign in
+                                </button>
+                            </form>
+                        </div>
                     </div>
-                    <button type="submit" class="btn btn-primary">Sign in</button>
-                </form>
+                </div>
             </div>
         </div>
 
--- signup.php
+++ signup.php
@@ -13,83 +13,177 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LeaveBoard - Sign up</title>
-    <link rel="stylesheet" href="./css/auth/signup.css">
-</head>
-<body>
-    <!-- Header -->
-    <?php
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>LeaveBoard - Sign up</title>
+        <link rel="stylesheet" href="./css/auth/signup.css" />
+        <style>
+            .form-label {
+                margin-bottom: 0rem !important;
+            }
+        </style>
+    </head>
+    <body>
+        <!-- Header -->
+        <?php
         @include('./partials/header.php');
     ?>
 
-    <!-- Content -->
-    <div class="container">
-        <div class="row">
-            <form method="POST">
-                <div class="mb-3">
-                    <label for="firstName" class="form-label">First Name</label>
-                    <input
-                        type="text"
-                        name="firstName"
-                        class="form-control"
-                        id="firstName"
-                    />
+        <!-- Content -->
+        <div class="container">
+            <div class="card card-body mt-5">
+                <div class="row">
+                    <div class="col-md-6 col-sm-12">
+                        <div
+                            class="d-flex justify-content-center align-items-center"
+                        >
+                            <img
+                                src="./img/signup-side-img.jpg"
+                                alt="Login page side photo"
+                                class="w-100"
+                            />
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12">
+                        <div class="d-flex flex-column">
+                            <div class="align-items-center text-center">
+                                <img
+                                    src="./img/logo.png"
+                                    alt="Logo"
+                                    width="120px"
+                                    class="mt-2 mb-5"
+                                />
+                            </div>
+                            <div>
+                                <h2
+                                    class="fw-bold mb-0"
+                                    style="font-size: 1.2rem"
+                                >
+                                    Online Leave Management System
+                                </h2>
+                                <p class="text-muted">Create a new account</p>
+                            </div>
+                            <form method="POST">
+                                <div class="mb-2">
+                                    <label for="firstName" class="form-label"
+                                        >First Name</label
+                                    >
+                                    <input
+                                        type="text"
+                                        name="firstName"
+                                        class="form-control"
+                                        id="firstName"
+                                        placeholder="i.e. Henry"
+                                    />
+                                </div>
+                                <div class="mb-2">
+                                    <label for="lastName" class="form-label"
+                                        >Last Name</label
+                                    >
+                                    <input
+                                        type="text"
+                                        name="lastName"
+                                        class="form-control"
+                                        id="lastName"
+                                        placeholder="i.e. Cavill"
+                                    />
+                                </div>
+                                <div class="mb-2">
+                                    <label for="email" class="form-label"
+                                        >Email address</label
+                                    >
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        class="form-control"
+                                        id="email"
+                                        placeholder="i.e. henrycavill@domain.com"
+                                    />
+                                </div>
+                                <div class="mb-2">
+                                    <label for="password" class="form-label"
+                                        >Password</label
+                                    >
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        class="form-control"
+                                        id="password"
+                                        placeholder="A strong password"
+                                    />
+                                </div>
+                                <div class="mb-2">
+                                    <label
+                                        for="confirmPassword"
+                                        class="form-label"
+                                        >Confirm Password</label
+                                    >
+                                    <input
+                                        type="password"
+                                        name="confirmPassword"
+                                        class="form-control"
+                                        id="confirmPassword"
+                                        placeholder="Reenter the password"
+                                    />
+                                </div>
+                                <div class="mb-2">
+                                    <label for="role" class="form-label"
+                                        >Role</label
+                                    >
+                                    <select
+                                        class="form-select"
+                                        name="role"
+                                        aria-label="role"
+                                    >
+                                        <option value="" disabled selected>
+                                            -Select Role-
+                                        </option>
+                                        <option value="hr">HR</option>
+                                        <option value="employee">
+                                            Employee
+                                        </option>
+                                    </select>
+                                </div>
+                                <div class="form-check mb-2">
+                                    <input
+                                        class="form-check-input"
+                                        value=""
+                                        type="checkbox"
+                                        id="agreement"
+                                        name="agreement"
+                                    />
+                                    <label
+                                        class="form-check-label"
+                                        for="agreement"
+                                    >
+                                        Accept terms and conditions
+                                    </label>
+                                </div>
+                                <div class="mb-2">
+                                    <p class="text-muted">
+                                        Already have an account?
+                                        <a href="signin.php" class="text-dark"
+                                            >Signin to your existing account!</a
+                                        >
+                                    </p>
+                                </div>
+                                <button
+                                    type="submit"
+                                    class="form-control btn btn-dark text-uppercase"
+                                >
+                                    Sign up
+                                </button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <div class="mb-3">
-                    <label for="lastName" class="form-label">Last Name</label>
-                    <input
-                        type="text"
-                        name="lastName"
-                        class="form-control"
-                        id="lastName"
-                    />
-                </div>
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email address</label>
-                    <input
-                        type="email"
-                        name="email"
-                        class="form-control"
-                        id="email"
-                    />
-                </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Password</label>
-                    <input
-                        type="password"
-                        name="password"
-                        class="form-control"
-                        id="password"
-                    />
-                </div>
-                <div class="mb-3">
-                    <label for="confirmPassword" class="form-label">Confirm Password</label>
-                    <input
-                        type="password"
-                        name="confirmPassword"
-                        class="form-control"
-                        id="confirmPassword"
-                    />
-                </div>
-                <div class="mb-3">
-                    <label for="role" class="form-label">Role</label>
-                    <select class="form-select" name="role" aria-label="role">
-                        <option value="" disabled selected>-Select Role-</option>
-                        <option value="hr">HR</option>
-                        <option value="employee">Employee</option>
-                    </select>
-                </div>
-                <button type="submit" class="btn btn-primary">Login</button>
-            </form>
+            </div>
         </div>
-    </div>
 
-    <!-- Footer -->
-    <?php
+        <!-- Footer -->
+        <?php
         @include('./partials/footer.php');
     ?>
-</body>
+    </body>
 </html>
